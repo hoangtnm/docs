3948442ac1f5d1d78af661517a451edfc92736fa
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-10-28 17:29:00 +0700

    Update install-intel-mkl-from_source.sh

--- Building_from_source/install-intel-mkl-from_source.sh
+++ Building_from_source/install-intel-mkl-from_source.sh
@@ -18,16 +18,13 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	sudo apt install -y libatlas-base-dev gfortran
 	sudo apt install -y python2.7-dev python3-dev
 	
-	export OPENCV_VERSION=3.4.3
-	export OPENCV_DOWNLOAD_URL=https://github.com/opencv/opencv/archive/$OPENCV_VERSION.zip
-	export OPENCV_CONTRIB_DOWNLOAD_URL=https://github.com/opencv/opencv_contrib/archive/$OPENCV_VERSION.zip
+	export INTEL_MKL_VERSION=0.16
+	export INTEL_MKL_DOWNLOAD_URL=https://github.com/intel/mkl-dnn/archive/v$INTEL_MKL_VERSION.zip
   
 	export CURRENT_DIR=`pwd`
-	wget "$OPENCV_DOWNLOAD_URL" -O opencv.zip
-	wget "$OPENCV_CONTRIB_DOWNLOAD_URL" -O opencv_contrib.zip
-	unzip opencv.zip
-	unzip opencv_contrib.zip
-	cd opencv-$OPENCV_VERSION/
+	wget "$INTEL_MKL_DOWNLOAD_URL" -O mkl-dnn.zip
+	unzip mkl-dnn.zip
+	cd mkl-dnn-$INTEL_MKL_VERSION/
 	mkdir build
 	cd build
 	cmake -D CMAKE_BUILD_TYPE=RELEASE \
