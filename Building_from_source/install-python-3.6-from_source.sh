569cfd10e5409f18f07f5d0a610363bef2c62b31
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-10-29 21:57:13 +0700

    Update install-python-3.6-from_source.sh

--- Building_from_source/install-python-3.6-from_source.sh
+++ Building_from_source/install-python-3.6-from_source.sh
@@ -2,9 +2,9 @@
 
 echo ""
 echo "******************** Please confirm ***************************"
-echo " Installing CUDA Toolkit may take a long time. "
-echo " Select n to skip CUDA Toolkit installation or y to install it." 
-read -p " Continue installing CUDA Toolkit (y/n) ? " CONTINUE
+echo " Installing Python 3.6 may take a long time. "
+echo " Select n to skip Python 3.6 installation or y to install it." 
+read -p " Continue installing Python 3.6 (y/n) ? " CONTINUE
 if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	echo "";
 	echo "Installing Python 3.6.5";
