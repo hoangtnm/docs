6cefc1868d8d0c22997315c81b0d1eab0d26a44d
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-10-29 21:56:16 +0700

    Create install-python-3.6-from_source.sh

---
+++ Building_from_source/install-python-3.6-from_source.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+echo ""
+echo "******************** Please confirm ***************************"
+echo " Installing CUDA Toolkit may take a long time. "
+echo " Select n to skip CUDA Toolkit installation or y to install it." 
+read -p " Continue installing CUDA Toolkit (y/n) ? " CONTINUE
+if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
+	echo "";
+	echo "Installing Python 3.6.5";
+	echo "";
+	echo "Downloading and Building the the Source Code";
+	echo "";
+	export PYTHON_VERSION=3.6.5
+	export PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+	sudo apt update && sudo apt install --no-install-recommends -y \
+  software-properties-common build-essential \
+  dpkg-dev libssl-dev libreadline-dev libbz2-dev libsqlite3-dev zlib1g-dev python-tk python3-tk tk-dev \
+  python-minimal
+	wget "$PYTHON_DOWNLOAD_URL" -O python.tar.tgz
+	tar -zxvf python.tar.tgz
+	cd Python-$PYTHON_VERSION
+	./configure --enable-shared --enable-ipv6 --enable-optimizations --enable-loadable-sqlite-extensions 
+	make -j $(nproc)
+	echo "";
+	echo "Finalizing the Installation"
+	echo "";
+	sudo make install -j $(nproc)
+	echo "Updating pip3 to the latest version";
+	echo "";
+	sudo rm /usr/bin/python && sudo ln -s /usr/local/bin/python3 /usr/bin/python
+	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+	sudo python3 get-pip.py
+else
+	echo "";
+	echo "Skipping Python 3.6.5 installation";
+	echo "";
+fi
