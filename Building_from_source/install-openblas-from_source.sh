ec597be3f4256f5d47e056a9ddf5bedd530fdf14
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-10-30 16:35:49 +0700

    Create install-openblas-from_source.sh

---
+++ Building_from_source/install-openblas-from_source.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+echo ""
+echo "******************** Please confirm ***********************"
+echo " Installing OpenBLAS from source may take a long time. "
+echo " Select n to skip OpenBLAS installation or y to install it." 
+read -p " Continue installing OpenBLAS (y/n) ? " CONTINUE
+if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
+	sudo apt update -y && sudo apt-get upgrade -y
+	sudo apt install -y build-essential cmake cython git gcc g++ gfortran
+	
+	echo "";
+	echo "Downloading and Building the Source Code";
+	echo "";
+	git clone https://github.com/xianyi/OpenBLAS.git
+	cd OpenBLAS
+	make FC=gfortran -j $(nproc)
+	sudo make install
+	
+	echo "";
+	echo "Finalizing the Installation";
+	echo "";
+	rm -rf OpenBLAS
+	echo 'export LD_LIBRARY_PATH=/opt/OpenBLAS/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+	sudo sh -c "echo '/opt/OpenBLAS/lib' > /etc/ld.so.conf.d/openblas.conf"
+	source ~/.bashrc
+	sudo ldconfig
+else
+	echo "";
+	echo "Skipping OpenBLAS installation";
+	echo "";
+fi
