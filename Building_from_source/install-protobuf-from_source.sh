93f356fef6c9d60efab65fea5c7ea8d4e79d260d
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-10-31 15:40:04 +0700

    Create install-protobuf-from_source.sh

---
+++ Building_from_source/install-protobuf-from_source.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+echo ""
+echo "************************ Please confirm *******************************"
+echo " Installing Protobuf from source may take a long time. "
+echo " Select n to skip Protobuf installation or y to install it." 
+read -p " Continue installing Protobuf (y/n) ? " CONTINUE
+if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
+	echo "";
+	echo "Installing Protobuf"; 
+	echo "";
+	sudo apt update -y && sudo apt-get upgrade -y
+	sudo apt install -y build-essential autoconf automake libtool curl make g++ unzip
+	
+	export PROTOBUF_VERSION=3.6.1
+	export PROTOBUF_DOWNLOAD_URL=https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-all-$PROTOBUF_VERSION.zip
+	
+	echo "";
+	echo "Downloading and Building the Source Code";
+	echo "";
+	wget "$PROTOBUF_DOWNLOAD_URL" -O protobuf.zip
+	unzip protobuf.zip
+	cd protobuf-$PROTOBUF_VERSION
+	./configure
+	make -j $(nproc)
+	make check -j $(nproc)
+	sudo make install
+	sudo ldconfig
+	
+	echo "";
+	echo "Finalizing the Installation";
+	echo "By default, the package will be installed to /usr/local"
+	echo "";
+	echo 'export LD_LIBRARY_PATH=/usr/local/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+	source ~/.bashrc
+	echo "";
+else
+	echo "";
+	echo "Skipping Protobuf installation";
+	echo "";
+fi
