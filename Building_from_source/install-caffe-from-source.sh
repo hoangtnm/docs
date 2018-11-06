36afa8a5650d41c9f4416794f251f978217a1fb3
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-11-06 14:52:01 +0700

    Update install-caffe-from-source.sh

--- Building_from_source/install-caffe-from-source.sh
+++ Building_from_source/install-caffe-from-source.sh
@@ -28,12 +28,14 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	sed -i 's/# USE_CUDNN := 1/USE_CUDNN := 1/g' Makefile.config
 	sed -i 's/# OPENCV_VERSION := 3/OPENCV_VERSION := 3/g' Makefile.config
 	sed -i 's/# CUSTOM_CXX := g++/CUSTOM_CXX := g++/g' Makefile.config
-	sed -i 's/# BLAS_INCLUDE := /path/to/your/blas//opt/OpenBLAS/include/g' Makefile.config
-	sed -i 's/# BLAS_LIB := /path/to/your/blas/opt/OpenBLAS/lib/g' Makefile.config
-	sed -i 's/PYTHON_INCLUDE := /usr/include/python2.7/#PYTHON_INCLUDE := /usr/include/python2.7/g' Makefile.config
-	sed -i 's/# PYTHON_INCLUDE := /usr/include/python3.5m/PYTHON_INCLUDE := /usr/include/python3.6m/g' Makefile.config
-	sed -i 's/#                 /usr/lib/python3.5/dist-packages/                 /usr/lib/python3.5/site-packages/g' Makefile.config
-	sed -i 's/INCLUDE_DIRS := $(PYTHON_INCLUDE) /usr/local/include/
+	sed -i 's/# BLAS_INCLUDE := \/path\/to\/your\/blas/opt\/OpenBLAS\/include/g' Makefile.config
+	sed -i 's/# BLAS_LIB := \/path\/to\/your\/blas/opt\/OpenBLAS\/lib/g' Makefile.config
+	sed -i 's/PYTHON_INCLUDE := \/usr\/include\/python2.7/#PYTHON_INCLUDE := \/usr\/include\/python2.7/g' Makefile.config
+	sed -i 's/# PYTHON_LIBRARIES := boost_python3 python3.5m/PYTHON_LIBRARIES := boost_python3 python3.6m' Makefile.config
+	sed -i 's/# PYTHON_INCLUDE := \/usr\/include\/python3.5m/PYTHON_INCLUDE := \/usr\/local\/local\/include\/python3.6m/g' Makefile.config
+	sed -i 's/#                 \/usr\/lib\/python3.5\/dist-packages/                 \/usr\/local\/lib\/python3.6\/site-packages/g' Makefile.config
+	sed -i 's/INCLUDE_DIRS := $(PYTHON_INCLUDE) \/usr\/locak\/include/INCLUDE_DIRS := $(PYTHON_INCLUDE) \/usr\/local\/include \/usr\/include\/hdf5\/serial' Makefile.config
+	sed -i 's/LIBRARY_DIRS := $(PYTHON_LIB) \/usr\/local\/lib \/usr\/lib/LIBRARY_DIRS := $(PYTHON_LIB) \/usr\/local\/lib \/usr\/lib\/x86_64-linux-gnu\/hdf5\/serial' Makefile.config
 else
 	echo "";
 	echo "Skipping Caffe installation";
