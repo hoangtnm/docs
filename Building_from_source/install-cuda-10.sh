31c33944309da10c1c82f1748cee21bf321dd34e
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-11-14 08:50:17 +0700

    Update PATH and LD_LIBRARY_PATH

--- Building_from_source/install-cuda-10.sh
+++ Building_from_source/install-cuda-10.sh
@@ -41,9 +41,9 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	sudo ln -s /usr/local/cuda-10.0 /usr/local/cuda
 	sudo apt-mark hold libnccl2
 	sudo apt-mark hold libcudnn7
-	echo 'export PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc
+	echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc
 	echo 'export LIBRARY_PATH=/usr/local/cuda/lib64/stubs${LIBRARY_PATH:+:${LIBRARY_PATH}}' >> ~/.bashrc
-	echo 'export LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+	echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
 	source ~/.bashrc
 	sudo ldconfig
 	echo "The installation completed!";
