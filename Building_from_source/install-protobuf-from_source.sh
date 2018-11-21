50b63a823a75317a9e90c355b602db8a95868e99
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-11-21 13:53:24 +0700

    Update install-protobuf-from_source.sh

--- Building_from_source/install-protobuf-from_source.sh
+++ Building_from_source/install-protobuf-from_source.sh
@@ -7,19 +7,22 @@ echo " Select n to skip Protobuf installation or y to install it."
 read -p " Continue installing Protobuf (y/n) ? " CONTINUE
 if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	echo "";
-	echo "Installing Protocol Compiler"; 
+	echo "Installing tools"; 
 	echo "";
 	sudo apt update -y
 	sudo apt install -y build-essential autoconf automake libtool curl make g++ unzip
 	
+	echo "";
+	echo "Downloading the Source Code";
+	echo "";
 	export PROTOBUF_VERSION=3.6.1
 	export PROTOBUF_DOWNLOAD_URL=https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-all-$PROTOBUF_VERSION.zip
-	
-	echo "";
-	echo "Downloading and Building the Source Code";
-	echo "";
 	wget "$PROTOBUF_DOWNLOAD_URL" -O protobuf.zip
 	unzip protobuf.zip
+	
+	echo "";
+	echo "Building and Installing the C++ Protocol Buffer runtime and the Protocol Buffer compiler (protoc)";
+	echo "";
 	cd protobuf-$PROTOBUF_VERSION
 	./configure
 	make -j $(nproc)
@@ -27,7 +30,7 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	sudo make install
 	
 	echo "";
-	echo "Finalizing the Installation";
+	echo "Refreshing shared library cache";
 	echo "By default, the Protocol Compiler will be installed to /usr/local";
 	echo "";
 	echo 'export LD_LIBRARY_PATH=/usr/local/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
@@ -41,6 +44,10 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	python setup.py build
 	python setup.py test
 	sudo python setup.py install
+	
+	echo "";
+	echo "The installation completed";
+	echo "";
 else
 	echo "";
 	echo "Skipping Protobuf installation";
