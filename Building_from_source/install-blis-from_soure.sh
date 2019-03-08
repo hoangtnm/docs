27ed2d793f4c39fb3457c6ca128594af6706a40e
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2019-03-08 16:01:42 +0700

    Update install-blis-from_soure.sh

--- Building_from_source/install-blis-from_soure.sh
+++ Building_from_source/install-blis-from_soure.sh
@@ -6,35 +6,25 @@ echo " Installing BLIS from source may take a long time. "
 echo " Select n to skip BLIS installation or y to install it." 
 read -p " Continue installing BLIS (y/n) ? " CONTINUE
 if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then  
-	echo "";
-	echo "Installing BLIS"; 
-	echo "";
+	echo "\nInstalling BLIS\n"; 
 	sudo apt update -y
 	sudo apt install -y build-essential cmake make git
 	
-	echo "";
-	echo "Downloading and Building the Source Code";
-	echo "";
+	echo "\nDownloading and Building the Source Code\n";
 	git clone https://github.com/flame/blis.git
 	cd blis
 	./configure --enable-cblas --enable-threading=openmp auto
 	make -j $(nproc)
 	
-	echo "";
-	echo "Validating the Build";
-	echo "";
+	echo "\nValidating the Build\n";
 	make check -j $(nproc)
 	
-	echo "";
-	echo "Finalizing the installation";
+	echo "\nFinalizing the installation\n";
 	echo "The default installation location is $HOME/blis";
 	echo "  libraries will be installed at $(HOME)/blis/lib";
-	echo "  development headers will be installed at $(HOME)/blis/include/blis";
-	echo "";
+	echo "  development headers will be installed at $(HOME)/blis/include/blis\n";
 	make install
 	bash -c 'echo "export LD_LIBRARY_PATH=$HOME/blis/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc'
 else
-	echo "";
-	echo "Skipping BLIS installation";
-	echo "";
+	echo "\nSkipping BLIS installation\n";
 fi
