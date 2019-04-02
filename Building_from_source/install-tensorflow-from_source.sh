0d51025227a0cd2bff61f90f3e26a178dd58080f
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2019-04-02 21:55:12 +0700

    Update install-tensorflow-from_source.sh

--- Building_from_source/install-tensorflow-from_source.sh
+++ Building_from_source/install-tensorflow-from_source.sh
@@ -23,12 +23,13 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	echo "";
 	echo "Install the TensorFlow pip package dependencies";
 	echo "";
-	sudo pip install --no-deps six mock keras_applications==1.0.6 keras_preprocessing==1.0.5
+	sudo pip install pip six numpy wheel setuptools mock
+	sudo pip install --no-deps keras_applications==1.0.6 keras_preprocessing==1.0.5
 	
 	echo "";
 	echo "Downloading the Source Code";
 	echo "";
-	export TENSORFLOW_VERSION=1.12
+	export TENSORFLOW_VERSION=1.13
 	git clone https://github.com/tensorflow/tensorflow.git
 	cd tensorflow
 	git checkout r$TENSORFLOW_VERSION
@@ -36,27 +37,7 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	echo "";
 	echo "Configuring the build";
 	echo "";
-	./configure << EOF
-	/usr/local/bin/python3
-	/usr/local/lib/python3.6/site-packages
-	\n
-	\n
-	\n
-	\n
-	y
-	10.0
-	\n
-	\n
-	\n
-	\n
-	1.3
-	\n
-	\n
-	\n
-	\n
-	-march=native
-	\n
-	EOF
+	./configure
 	
 	echo "";
 	echo "Making the TensorFlow package builder with GPU support";
@@ -71,7 +52,7 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 	echo "";
 	echo "Installing the package";
 	echo "";
-	sudo pip3 install --upgrade /tmp/tensorflow_pkg/tensorflow-1.12.0-cp36-cp36m-linux_x86_64.whl
+	sudo pip3 install --upgrade /tmp/tensorflow_pkg/tensorflow-1.13.1-cp36-cp36m-linux_x86_64.whl
 else
 	echo "";
 	echo "Skipping TensorFlow installation";
