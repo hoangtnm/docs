3fec689abcd77260543181505919151c4d6f2e73
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2019-05-02 17:40:15 +0700

    Update install-cuda-10.sh

--- Building_from_source/install-cuda-10.sh
+++ Building_from_source/install-cuda-10.sh
@@ -33,8 +33,9 @@ if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
 		libcudnn7=$CUDNN_VERSION-1+cuda10.0 \
 		libcudnn7-dev=$CUDNN_VERSION-1+cuda10.0
 	sudo ln -s /usr/local/cuda-10.0 /usr/local/cuda
-	sudo apt-mark hold libnccl2
-	sudo apt-mark hold libcudnn7
+	sudo apt-mark hold libnccl2 libnccl-dev
+	sudo apt-mark hold libcudnn7 libcudnn7-dev
+	
 	echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc
 	echo 'export LIBRARY_PATH=/usr/local/cuda/lib64/stubs${LIBRARY_PATH:+:${LIBRARY_PATH}}' >> ~/.bashrc
 	echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
