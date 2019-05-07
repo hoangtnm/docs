94fec3185a302142703994f4185cb5ccd2173d72
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2019-05-07 16:47:38 +0700

    Update install-nvidia-driver.sh

--- Building_from_source/install-nvidia-driver.sh
+++ Building_from_source/install-nvidia-driver.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 echo $'blacklist nouveau\noptions nouveau modeset=0' >> /etc/modprobe.d/disable-nouveau.conf
-sudo dracut --force
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+# sudo dracut --force
+# sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
-sudo apt install -y nvidia-driver-418
+sudo apt install -y nvidia-driver-430
