6dbb328e6a6762df2864fdd10b2a9114d56233ed
hoangtnm <minhhoangtrannhat@gmail.com>
2019-07-07 22:25:34 +0700

    init install_prebuilt_protoc.sh

---
+++ scripts/install_prebuilt_protoc.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+PROTOC_VERSION=3.7.1
+ARCHITECTURE = "x86_64"
+
+echo ""
+echo "************************ Please confirm *******************************"
+echo " Installing Protobuf $PROTOC_VERSION from source may take a long time. "
+echo " Select n to skip the installation or y to install it." 
+read -p " Continue installing Protobuf (y/n) ? " CONTINUE
+if [[ "$CONTINUE" == "y" || "$CONTINUE" == "Y" ]]; then
+
+    read -p " Select your OS's architecture [x86_64/aarch_64], default is $ARCHITECTURE " ARCHITECTURE_INPUT
+    if [[ "$ARCHITECTURE_INPUT" != "" ]]; then
+        if [[ "$ARCHITECTURE_INPUT" == "x86_64" || "$ARCHITECTURE_INPUT" == "aarch_64" ]]; then
+            $ARCHITECTURE = $ARCHITECTURE_INPUT
+        else
+            echo "Unsupported architecture!"
+            exit 0
+        fi
+    fi
+
+	echo "";
+	echo " Downloading the protocol compiler ";
+	echo "";
+    sudo apt update && sudo apt install wget
+    export OS="linux"
+    export PLATFORM=$OS-$ARCHITECTURE
+	export PROTOC_DOWNLOAD_URL=https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/protoc-$PROTOC_VERSION-$PLATFORM.zip
+	wget "$PROTOC_DOWNLOAD_URL" -O protoc.zip
+	unzip protoc.zip
+	
+    sudo cp protoc-$PROTOC_VERSION-$PLATFORM /usr/local
+    cp include/* /usr/local/include/
+	
+	echo "";
+	echo " Refreshing shared library cache ";
+	echo "By default, the Protocol Compiler will be installed to /usr/local";
+	echo "";
+    echo "export PATH=/usr/local/protoc-$PROTOC_VERSION-$PLATFORM"'${PATH:+:${PATH}}' >> ~/.bashrc
+	source ~/.bashrc
+	sudo ldconfig
+	
+	echo "";
+	echo "The installation completed";
+	echo "";
+else
+	echo "";
+	echo "Skipping Protoc installation";
+	echo "";
+fi
