4af2a53245c5344103be11033e4e58460f552c80
hoangtnm <minhhoangtrannhat@gmail.com>
2019-07-09 21:26:55 +0700

    Update install_ros.sh

--- scripts/install_ros.sh
+++ scripts/install_ros.sh
@@ -1,6 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "\n Configuring your Ubuntu repositories \n"
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+sudo apt-add-repository restricted
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
@@ -18,3 +21,6 @@ source ~/.bashrc
 if [[ -f ~/.zshrc ]]; then
     echo "source /opt/ros/melodic/setup.zsh" >> ~/.zshrc
 fi
+
+echo "\n Dependencies for building packages \n"
+sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
