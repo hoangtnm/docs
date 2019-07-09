2b54eaf893e03235726a368b9b8e3b94863691ce
hoangtnm <minhhoangtrannhat@gmail.com>
2019-07-09 21:33:26 +0700

    Update install_ros.sh

--- scripts/install_ros.sh
+++ scripts/install_ros.sh
@@ -8,7 +8,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 echo "\n Installing ros-melodic-desktop-full \n"
-sudo apt install ros-melodic-desktop-full
+sudo apt update && sudo apt install -y ros-melodic-desktop-full
 
 echo "\n Initializing rosdep \n"
 sudo rosdep init
