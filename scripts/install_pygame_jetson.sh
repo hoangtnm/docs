4f1a306acc06ecdf6baddd695e843dae2ba1690d
hoangtnm <minhhoangtrannhat@gmail.com>
2019-07-10 15:35:18 +0700

    init install_pygame_jetson.sh

---
+++ scripts/install_pygame_jetson.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y libsdl2-dev libfreetype6-dev libsdl2-mixer-dev libsdl2-image-dev libsdl2-ttf-dev libjpeg-dev libpng-dev libportmidi-dev \
+    python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion ffmpeg libswscale-dev libavformat-dev libavcodec-dev
+sudo pip instal pygame
