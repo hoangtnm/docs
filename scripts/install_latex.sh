c2bbcdc5c7d784effafae6861bea2feeb96bf4f5
hoangtnm <minhhoangtrannhat@gmail.com>
2019-08-08 09:48:44 +0700

    Add latex_installation

---
+++ scripts/install_latex.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LATEX_VERSION=2019
+export CURR_DIR=$pwd
+
+echo "Pre-installing: downloading, cleaning up" && \
+wget https://mirror-hk.koddos.net/CTAN/systems/texlive/Images/texlive.iso && \
+rm -rf /usr/local/texlive/$LATEX_VERSION && \
+rm -rf ~/.$LATEX_VERSION
+
+echo "\nRunning the installer\n" && \
+mount -t iso9660 -o ro,loop,noauto $CURR_DIR/texlive.iso /mnt && \
+cd /mnt/ && \
+./install-tl && \
+echo "[... messages omitted ...]" && \
+echo "Enter command: i" && \
+echo "[... when done, see below for post-install ...]"
+
+echo "Post-installing: setting PATH" && \
+ehco 'PATH=/usr/local/texlive/2019/bin/x86_64-linux${PATH:+:${PATH}}' >> ~/.bashrc
+if [[ -f ~/.zshrc ]]; then
+		ehco 'PATH=/usr/local/texlive/2019/bin/x86_64-linux${PATH:+:${PATH}}' >> ~/.zshrc
+fi
