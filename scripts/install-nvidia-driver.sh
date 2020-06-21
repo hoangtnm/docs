a00103cbb4cee561ea95db9d0c7a09a9e326ba7a
hoangtnm <minhhoangtrannhat@gmail.com>
2020-06-21 16:17:32 +0700

    feat: update NVIDIA-Driver script

--- scripts/install-nvidia-driver.sh
+++ scripts/install-nvidia-driver.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo ""
-echo "Disabling Nouveau"
-echo ""
+echo
+echo Disabling Nouveau
+echo
 cat <<EOF >> /etc/modprobe.d/disable-nouveau.conf
 blacklist nouveau
 options nouveau modeset=0
@@ -11,7 +11,7 @@ EOF
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update && sudo apt-get install -y nvidia-driver-440
 
-echo ""
-echo "Regenerating the kernel initramfs"
-echo ""
+echo
+echo Regenerating the kernel initramfs
+echo
 sudo update-initramfs -u
