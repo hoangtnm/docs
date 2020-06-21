92f86620ef38c402e7cda959cd851af8b1b0c700
hoangtnm <minhhoangtrannhat@gmail.com>
2020-06-21 16:18:05 +0700

    feat: update Bazel version to 3.99

--- scripts/install-bazel.sh
+++ scripts/install-bazel.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-echo "Installing JDK 8";
-sudo apt-get install -y curl openjdk-8-jdk
+echo Installing JDK 8
+sudo apt-get update && sudo apt-get install -y curl openjdk-8-jdk
 
-echo "Installing required packages";
+echo Installing dependencies
 sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip
 
-echo "\nInstalling Bazel 0.21\n";
-export BAZEL_VERSION=0.21.0
-export BAZEL_DOWNLOAD_URL=https://github.com/bazelbuild/bazel/releases/download/$BAZEL_VERSION/bazel-$BAZEL_VERSION-installer-linux-x86_64.sh
-wget $BAZEL_DOWNLOAD_URL
-chmod +x bazel-$BAZEL_VERSION-installer-linux-x86_64.sh
-./bazel-$BAZEL_VERSION-installer-linux-x86_64.sh --user
+VERSION=3.99.0
+echo Installing Bazel $VERSION
+DOWNLOAD_URL=https://github.com/bazelbuild/bazel/releases/download/$VERSION/bazel-$VERSION-installer-linux-x86_64.sh
+wget $DOWNLOAD_URL
+chmod +x bazel-$VERSION-installer-linux-x86_64.sh
+./bazel-$VERSION-installer-linux-x86_64.sh --user
 
-echo "\nSet up your environment\n";
+echo Adding bazel to PATH
 echo 'export PATH=$HOME/bin${PATH:+:${PATH}}' >> ~/.bashrc
 source ~/.bashrc
-
-echo "\nThe installation completed\n"
