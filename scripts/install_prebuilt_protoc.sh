6c7f0fc98aeca290137b1018e4ca51ff2c48ce1f
hoangtnm <minhhoangtrannhat@gmail.com>
2020-06-25 12:11:05 +0700

    refactor: install_prebuilt_protoc.sh

--- scripts/install_prebuilt_protoc.sh
+++ scripts/install_prebuilt_protoc.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
 
-echo
-echo Downloading the protocol compiler
-echo
+# 
+#  Download Protocol Compiler, which is used to compile .proto files
+#
 
 VERSION=3.7.1
 PLATFORM=linux-$(arch)
 DOWNLOAD_URL=https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/protoc-$VERSION-$PLATFORM.zip
 
 sudo apt-get update && sudo apt-get install -y wget
-wget "$DOWNLOAD_URL" -O protoc-$VERSION.zip
+wget $DOWNLOAD_URL -O protoc-$VERSION.zip
 unzip protoc-$VERSION.zip -d protoc-$VERSION
 
-sudo cp -r protoc-$VERSION /usr/local
-sudo cp -r protoc-$VERSION/include/* /usr/local/include/
+sudo cp protoc-$VERSION/bin/* /usr/local
+sudo cp protoc-$VERSION/include/* /usr/local/include
 
-echo Refreshing shared library cache
-echo By default, the Protocol Compiler will be installed to /usr/local
-echo "export PATH=/usr/local/protoc-$VERSION/bin"'${PATH:+:${PATH}}' >> ~/.bashrc
+# 
+# Clean intermediate files
+# 
 
-if [[ -f ~/.zshrc ]]; then
-	echo "export PATH=/usr/local/protoc-$VERSION/bin"'${PATH:+:${PATH}}' >> ~/.zshrc
-fi
-
-sudo ldconfig
+rm -r protoc-$VERSION*
