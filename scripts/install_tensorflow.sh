51d1abf906a3f995214326b49f5ae8fff7c4ff93
hoangtnm <minhhoangtrannhat@gmaail.com>
2020-12-13 13:59:44 +0700

    feat: update TF script

--- scripts/install_tensorflow.sh
+++ scripts/install_tensorflow.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-VERSION='2.2'
+VERSION='2.3'
 
-# Uninstalling pip installation
+# Uninstall existing TensorFlow packages 
 python3 -m pip uninstall tensorflow*
 
-#
-# Installing dependencies
-#
-
+# Install dependencies
 apt-get update && apt-get install -y \
 	build-essential git
 python3 -m pip install -U \
@@ -24,17 +21,21 @@ cd tensorflow
 # Configure the build
 # See details at
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/dockerfiles/tests/build-gpu.sh
-
-export TF_NEED_GCP=1
-export TF_NEED_HDFS=1
-export TF_NEED_S3=1
+export TF_CUDA_VERSION=10.2
+export TF_CUDA_COMPUTE_CAPABILITIES="3.5,6.1,7.0,7.5,8.6"
+export TF_CUDA_PATHS=/usr/local/cuda-10.2
+export TF_CUDNN_VERSION=7.6
+export TF_NEED_GCP=0
+export TF_NEED_HDFS=0
+export TF_NEED_S3=0
 export TF_NEED_CUDA=1
+export PYTHON_BIN_PATH="$(which python3)"
 export TF_NEED_TENSORRT=0
 yes "" | python3 configure.py
 
-# Build TensorFlow package with GPU support
-bazel build --copt=-march=native --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0" --config=opt --config=v2 tensorflow/tools/pip_package:build_pip_package
+# Build TensorFlow package
+bazel build --copt=-march=native --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0" --config=opt tensorflow/tools/pip_package:build_pip_package
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 
-# Install the built package
+# Install the package
 python3 -m pip --no-cache-dir install --upgrade /tmp/tensorflow_pkg/*.whl
