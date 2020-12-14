e1ea98096c080eac6f54f881864b79752c4c4306
hoangtnm <minhhoangtrannhat@gmaail.com>
2020-12-14 23:22:51 +0700

    feat: update TF_CUDA_COMPUTE_CAPABILITIES

--- scripts/install_tensorflow.sh
+++ scripts/install_tensorflow.sh
@@ -21,8 +21,9 @@ cd tensorflow
 # Configure the build
 # See details at
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/dockerfiles/tests/build-gpu.sh
+# https://developer.nvidia.com/cuda-gpus
 export TF_CUDA_VERSION=10.2
-export TF_CUDA_COMPUTE_CAPABILITIES="3.5,6.1,7.0,7.5"
+export TF_CUDA_COMPUTE_CAPABILITIES="6.1,7.5"
 export TF_CUDA_PATHS="/usr/local/cuda-10.2,/usr/lib/x86_64-linux-gnu,/usr/include"
 export TF_CUDNN_VERSION=7
 export TF_NCCL_VERSION=2
