c908ae230a737418a0f724903a253a425a4f812d
hoangtnm <minhhoangtrannhat@gmail.com>
2020-12-21 15:03:27 +0700

    feat: update install_python3 script

--- scripts/install_python3.sh
+++ scripts/install_python3.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-#
 # Build Python 3 from source and install it.
-#
 # Usage: install_python3 [version]
 
 set -e
 
-VERSION=${1:-3.7.8}
+VERSION="3.8.5"
 DOWNLOAD_URL="https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz"
 
 echo "Downloading Python ${VERSION} source code"
@@ -32,11 +30,10 @@ cd "Python-${VERSION}"
 	--with-threads
 make -j $(nproc)
 make altinstall
-# make install
 
-update-alternatives --install /usr/local/bin/python python /usr/local/bin/python3.7 30
-update-alternatives --install /usr/local/bin/python3 python3 /usr/local/bin/python3.7 30
+update-alternatives --install /usr/local/bin/python python /usr/local/bin/python3.8 38
+update-alternatives --install /usr/local/bin/python3 python3 /usr/local/bin/python3.8 38
 ldconfig
 
 echo 'Installing the latest version of pip3'
-python3 -m pip install -U pip
+curl "https://bootstrap.pypa.io/get-pip.py" | python
